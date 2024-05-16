c1132e95c6cf73a4cb849104e4e742ee0038478c
Brandon Harker <brandon.harker@octanner.com>
2024-05-15 18:43:33 -0600

    Change attribute name

--- main.tf
+++ main.tf
@@ -16,14 +16,14 @@ data "aws_ami" "app_ami" {
 
 resource "aws_vpc" "learn-vpc" {
   default = false 
-  vpc_id = "vpc-0d5c2eb825eb54628"
+  id = "vpc-0d5c2eb825eb54628"
 }
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   # vpc_security_group_ids = ["sg-046f0acca3658f6fc"]
-  vpc_id        = aws_vpc.learn-vpc.id
+  id        = aws_vpc.learn-vpc.id
   subnet_id     = "subnet-02ca2edd2921b5488"
 
   tags = {
