511704da5db186c676e1a3dd685be2709f30a69e
Brandon Harker <brandon.harker@octanner.com>
2024-05-15 18:59:28 -0600

    Reverts to what was working

--- main.tf
+++ main.tf
@@ -14,15 +14,11 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_vpc" "learn-vpc" {
-  id = "vpc-0d5c2eb825eb54628"
-}
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  # vpc_security_group_ids = ["sg-046f0acca3658f6fc"]
-  id        = aws_vpc.learn-vpc.id
+  vpc_security_group_ids = ["sg-046f0acca3658f6fc"]
+  # id        = aws_vpc.learn-vpc.id
   subnet_id     = "subnet-02ca2edd2921b5488"
 
   tags = {
